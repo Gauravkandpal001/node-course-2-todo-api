402b8d5593027056008467b71b559d89b7a79c29
Gaurav <kandpalgaurav241@gmail.com>
2018-02-04 21:58:18 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
    todo.save().then((doc)=>{
      res.send(doc);
    },(e)=>{
-     res.send(e);
+     res.status(400).send(e);
    });
  });
 
@@ -25,3 +25,4 @@ app.use(bodyParser.json());
 app.listen(3000,() =>{
   console.log('Started on port 3000');
 });
+module.exports={app};

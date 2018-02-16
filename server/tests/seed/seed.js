0608f685317eb97ad99dcb66599be71212c0d6be
Gaurav <kandpalgaurav241@gmail.com>
2018-02-16 16:44:08 +0530

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,7 +17,11 @@ const users=[{
 },{
   _id:userTwoId,
   email:'jen@example.com',
-  password:'userTwoPass'
+  password:'userTwoPass',
+  tokens:[{
+    access:'auth',
+    token:jwt.sign({_id:userTwoId, access:'auth'},'abc123').toString()
+  }]
 }];
 
 
@@ -25,12 +29,14 @@ const users=[{
 
 const todos=[{
   _id:new ObjectID(),
-  text:'First test todo'
+  text:'First test todo',
+  _creator:userOneId
 },{
   _id:new ObjectID(),
   text:'Second test todo',
   completed:true,
-  completedAt:333
+  completedAt:333,
+  _creator:userTwoId
 }];
 //*********************************************
 
